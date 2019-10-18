7eb53015ce3bfef82d7f8a459b419dbe1f5d1e96
Peter O'Reilly <oreilp25@gmail.com>
2019-10-18 14:24:31 +0100

    add insert and find

---
+++ test/index.spec.js
@@ -0,0 +1,78 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable func-names */
+
+const { expect } = require('chai');
+const BinarySearchTree = require('../index.js');
+
+describe('BST', function () {
+    it('should create an instance', function () {
+        const bst = new BinarySearchTree();
+        expect(bst).to.be.instanceOf(BinarySearchTree);
+    });
+
+    it('should insert a root node', function () {
+        const bst = new BinarySearchTree();
+        bst.insert(123, { somekey: 'someValue'});
+        expect(bst.root.key).to.equal(123);
+    });
+
+    it('should insert multiple nodes', function () {
+        const bst = new BinarySearchTree();
+        bst.insert(123, { somekey: 'someValue'});
+    });
+
+    it('should insert a single child of root', function () {
+        const bst = new BinarySearchTree();
+        bst.insert(123, { somekey: 'someValue'});
+        bst.insert(124, { somekey: 'someOtherValue'});
+        const root = bst.find(123);
+        const child = bst.find(124);
+        expect(root.childNodes.right).to.deep.equal(child);
+    });
+
+    it('should insert two children of root', function () {
+        const bst = new BinarySearchTree();
+        bst.insert(123, {});
+        bst.insert(10000, {});
+        bst.insert(1, {});
+        const root = bst.root;
+        const rightChild = bst.find(10000);
+        const leftChild = bst.find(1);
+        expect(root.childNodes.right).to.deep.equal(rightChild);
+        expect(root.childNodes.left).to.deep.equal(leftChild);
+    });
+
+//              5
+//             / \
+//            /   \  
+//           3     8
+//          / \   / \
+//         1   4 6   9
+    it('should insert many children correctly', function () {
+        const bst = new BinarySearchTree();
+        bst.insert(5, {}); // root
+        bst.insert(3, {}); // N1 - root left-child
+        bst.insert(8, {}); // N2 - root right-child
+        bst.insert(1, {}); // N3 - N1 left-child
+        bst.insert(4, {}); // N4 - N1 right-child
+        bst.insert(6, {}); // N5 - N2 left-child
+        bst.insert(9, {}); // N6 - N2 right-child
+
+        const root = bst.root;
+        const nodeOne = bst.find(3);
+        const nodeTwo = bst.find(8);
+        const nodeThree = bst.find(1);
+        const nodeFour = bst.find(4);
+        const nodeFive = bst.find(6);
+        const nodeSix = bst.find(9);
+        expect(root.childNodes.left).to.deep.equal(nodeOne);
+        expect(root.childNodes.right).to.deep.equal(nodeTwo);
+        expect(nodeOne.childNodes.left).to.deep.equal(nodeThree);
+        expect(nodeOne.childNodes.right).to.deep.equal(nodeFour);
+        expect(nodeTwo.childNodes.left).to.deep.equal(nodeFive);
+        expect(nodeTwo.childNodes.right).to.deep.equal(nodeSix);        
+    });
+
+    //TODO Error cases:
+        // find where id does not exist!
+});
---
+++ test/node.spec.js
@@ -0,0 +1,115 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable func-names */
+
+const { expect } = require('chai');
+const Node = require('../lib/Node.js');
+const NodeComparer = require('../lib/NodeComparer.js');
+
+describe('BST Node', function () {
+    it('should create an instance', function () {
+        const node = new Node();
+        expect(node).to.be.instanceOf(Node);
+    });
+
+    it('should create a node with a key (identifier)', function () {
+        const node = new Node({ id: 'someid', someprop: '' });
+        node.key = 123;
+        expect(node.key).to.equal(123);
+        expect(node.attributes).to.deep.equal({ id: 'someid', someprop: '' });
+    });
+
+    it('should return child nodes from a node', function () {
+        const node = new Node();
+        node.key = 'doesntmatter';
+        const children = node.childNodes;
+        expect(children).to.deep.equal({ left: null, right: null });
+        expect(node.getChildAt(-1)).to.equal(null);
+        expect(node.getChildAt(1)).to.equal(null);
+    });
+
+    it('should set a child node to left', function () {
+        const node = new Node();
+        const child = new Node();
+        const children = node.setChild(-1, child);
+        expect(children.left).to.deep.equal(child);
+        expect(children.right).to.equal(null);
+        expect(node.getChildAt(-1)).to.deep.equal(child);
+        expect(node.getChildAt(1)).to.equal(null);
+    });
+
+    it('should set a child node to right', function () {
+        const node = new Node();
+        const child = new Node();
+        const children = node.setChild(1, child);
+        expect(children.left).to.deep.equal(null);
+        expect(children.right).to.equal(child);
+    });
+
+});
+
+describe('BST NodeComparer', function () {
+    it('should return 0 where compared node is equal', function () {
+        const comparer = new NodeComparer();
+        const nodeOne = new Node(123);
+        const nodeTwo = new Node(123);
+        const result = comparer.compare(nodeOne.id, nodeTwo.id);
+        expect(result).to.equal(0);
+    });
+
+    it('should return -1 where compared nodeOne is less than nodeTwo', function () {
+        const comparer = new NodeComparer();
+        const nodeOne = new Node();
+        const nodeTwo = new Node();
+        nodeOne.key = 123;
+        nodeTwo.key = 124;
+        const result = comparer.compare(nodeOne.key, nodeTwo.key);
+        expect(result).to.equal(-1);
+    });
+
+    it('should return +1 where compared nodeOne is greater than nodeTwo', function () {
+        const comparer = new NodeComparer();
+        const nodeOne = new Node();
+        const nodeTwo = new Node();
+        nodeOne.key = 124;
+        nodeTwo.key = 123;
+        const result = comparer.compare(nodeOne.key, nodeTwo.key);
+        expect(result).to.equal(1);
+    });
+
+    it('should return 0 where compared node is equal with custom comparer', function () {
+        const compareFunction = (n1, n2) => ((n1.someKey === n2.someKey) ? 0 : -2);
+        const comparer = new NodeComparer({ compareFunction });
+        const nodeOne = new Node();
+        const nodeTwo = new Node();
+        nodeOne.key = { someKey: 'samekey' };
+        nodeTwo.key = { someKey: 'samekey' };
+        const result = comparer.compare(nodeOne.key, nodeTwo.key);
+        expect(result).to.equal(0);
+    });
+
+    it('should return -1 where compared nodeOne is less than nodeTwo with custom comparer', function () {
+        const compareFunction = (n1, n2) => (
+            (n1.someKey.length < n2.someKey.length) ? -1 : undefined
+        );
+        const comparer = new NodeComparer({ compareFunction });
+        const nodeOne = new Node();
+        const nodeTwo = new Node();
+        nodeOne.key = { someKey: 'key' };
+        nodeTwo.key = { someKey: 'biggerkey' };
+        const result = comparer.compare(nodeOne.key, nodeTwo.key);
+        expect(result).to.equal(-1);
+    });
+
+    it('should return +1 where compared nodeOne is greater than nodeTwo with custom comparer', function () {
+        const compareFunction = (n1, n2) => (
+            (n1.someKey.length < n2.someKey.length) ? -1 : 1
+        );
+        const comparer = new NodeComparer({ compareFunction });
+        const nodeOne = new Node({ someKey: 'biggerkey' });
+        const nodeTwo = new Node({ someKey: 'key' });
+        nodeOne.key = { someKey: 'biggerkey' };
+        nodeTwo.key = { someKey: 'key' };
+        const result = comparer.compare(nodeOne.key, nodeTwo.key);
+        expect(result).to.equal(1);
+    });
+});
